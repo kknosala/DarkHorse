61276bb0e6f1f0648a4ece7a65c3fd8b6f47217a
KSK <kurtknosala@gmail.com>
2019-07-30 21:15:23 -0600

    ombd ajax call setup

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
 
-    console.log('working');
+
+    $('#submit-search').click(function(){
+
+        event.preventDefault();
+
+        var title = $('#title-input').val().trim();
+        var actor = $('#actor-input').val().trim();
+        var year = $('#year-input').val().trim();
+        var genre = $('#search-genre').val();
+        var queryURL = "https://www.omdbapi.com/?t=" + title + "&apikey=trilogy";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response)
+        })
+    })
 
 
 
